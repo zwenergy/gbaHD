2670c15ed9e187b507f05958d44501c5bb04f09e
BBsan <bbsan2k@gmail.com>
2022-02-13 13:12:57 +0100

    Fix setting env variable

--- .github/get_bitstream_version.sh
+++ .github/get_bitstream_version.sh
@@ -2,4 +2,4 @@
 # echo ${$(gdd if=default_720p-2.bit skip=$(($( echo $(env LANG=LC_ALL ggrep -a -b -o -P "\x30\x01\xA0\x01" $1) | gcut -d: -f1)+4)) bs=1 count=8 2>&- | strings)%?}
 OFFSET=$(env LANG=LC_ALL grep -a -b -o -P "\x30\x01\xA0\x01" $1 | cut -d: -f1)
 VERSION=$(dd if=$1 skip=$(($OFFSET+4)) bs=1 count=4 2>&- | strings)
-echo "${VERSION}" >> $BITSTREAM_VERSION
+echo "BITSTREAM_VERSION=${VERSION}" >> $GITHUB_ENV
